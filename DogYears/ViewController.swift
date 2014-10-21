e892083172be63a964b142256a5a07350d8b8f1a
pvigilante <pvigilante@pvigilante-macbook.local>
2014-10-21 10:54:18 -0700

    first commit

--- DogYears/ViewController.swift
+++ DogYears/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var dogYearsLabel: UILabel!
+    @IBOutlet weak var humanYearTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertToDogYearsPressed(sender: UIButton) {
+        
+        var dogYearsDouble = Double((humanYearTextField.text as NSString).doubleValue)
+        var humanYearMultiplier:Double
+        var removeYears:Double
+        var finalYears:Double
+        if dogYearsDouble <= 2.0 {
+            humanYearMultiplier = 10.5
+            removeYears = 0.0
+            finalYears = dogYearsDouble * humanYearMultiplier
+        } else {
+            dogYearsDouble -= 2.0
+            humanYearMultiplier = 4
+            finalYears = (dogYearsDouble * humanYearMultiplier) + 21
+        }
+        
+        dogYearsLabel.text = "\(finalYears)"
+        
+        //Close keyboard
+        humanYearTextField.resignFirstResponder()
+        
+    }
 
 }
 
